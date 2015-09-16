8df7887c3d55f5cfc8079ffa9ab912f49e958746
inei <iancnei@gmail.com>
2015-09-16 15:07:28 -0700

    create working classes

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,30 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+
+	attr_accessor :cars
+
+  	def initialize
+  		@cars = [Car.new, Car.new]
+  		@cars.each do |car| 
+  			car.accelerate(rand(99) + 1)
+  		end
+  	end
+
+  	def winner
+  		if @cars[0].speed > @cars[1].speed
+  			return @cars[0]
+  		else
+  			return @cars[1]
+  		end
+  	end
+
+  	def loser
+  		if winner == @cars[0]
+  			return @cars[1]
+  		else
+  			return @cars[0]
+  		end
+  	end
+
 end
